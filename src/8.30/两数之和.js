65bc298df12797b7d4d18d5dea0d0582aaca6cce
XuJingfang <110262858+XuJingfang@users.noreply.github.com>
2022-08-30 23:41:21 +0100

    Add files via upload

---
+++ src/8.30/两数之和.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+    let hash = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (hash[target - nums[i]] !== undefined) {
+        return [i, hash[target - nums[i]]];
+    }
+    hash[nums[i]] = i;
+    }
+    return [];
+};
